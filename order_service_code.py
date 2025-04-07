3bb8fb5e03103d2b3855e332d6ea3a1aa4d6a323
abhi <48319599+colmeabhi@users.noreply.github.com>
2025-04-08 00:42:41 +0530

    Update order_service_code.py
    added code comments for clarity

--- order_service_code.py
+++ order_service_code.py
@@ -2,7 +2,7 @@ import json, uuid, boto3
 dynamo = boto3.resource('dynamodb')
 sns = boto3.client('sns')
 order_table = dynamo.Table('OrderTable')
-SNS_TOPIC_ARN = 'arn:aws:sns:REGION:ACCOUNT_ID:OrderNotifications'
+SNS_TOPIC_ARN = 'arn:aws:sns:REGION:ACCOUNT_ID:OrderNotifications' # Insert your ARN here in the quotes !!!
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
         'quantity': body['quantity']
     })
     sns.publish(
-        TopicArn=SNS_TOPIC_ARN,
+        TopicArn=SNS_TOPIC_ARN, 
         Message=f"Order placed: {body['book']} (x{body['quantity']})",
         Subject="Book Order Confirmation"
     )
